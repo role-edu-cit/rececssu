2156418df6dcb9f9bd5c85dde33010bcc72df378
role-edu-cit <141157883+role-edu-cit@users.noreply.github.com>
2023-08-01 16:32:37 +0200

    Init commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#home').attr('src', 'https://fririk12.wixsite.com/rececssu');
+	//$('#home').attr('src', '');
+	
+	$('#proj').attr('src', 'https://fririk12.wixsite.com/rececssu/description-du-projet');
+	//$('#proj').attr('src', '');
+	
+	$('#prog').attr('src', 'https://fririk12.wixsite.com/rececssu/programme-de-travail');
+	//$('#prog').attr('src', '');
+	
+	$('#impact').attr('src', 'https://fririk12.wixsite.com/rececssu/impacts-potentiels-du-projet');
+	//$('#impact').attr('src', '');
+	
+	$('#conf').attr('src', 'https://fririk12.wixsite.com/rececssu/conf%C3%A9rences');
+	//$('#conf').attr('src', '');
+});
+
